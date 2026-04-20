756d10273212ec53e1f7c7861cc54e6547503954
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-04-20 09:31:33 +0530

    UC3

---
+++ src/srm/com/java/tictactoeUC3.java
@@ -0,0 +1,29 @@
+package srm.com.java;
+import java.util.Scanner;
+public class tictactoeUC3 {
+    public static void main(String[] args) {
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+    static int getUserSlot() {
+        Scanner scan = new Scanner(System.in);
+        int slot;
+        while (true) {
+            System.out.print("Enter a slot number (1-9): ");
+            if (scan.hasNextInt()) {
+                slot = scan.nextInt();
+                if (slot >= 1 && slot <= 9) {
+                    break; 
+                } else {
+                    System.out.println("Invalid input. Please enter a number between 1 and 9.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number between 1 and 9.");
+                scan.next(); 
+            }
+        }
+        scan.close();
+        return slot;
+    }
+}
+
