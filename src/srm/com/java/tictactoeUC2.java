63e41b6d325b72e242249a692af37419c168a9a6
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-04-20 09:19:26 +0530

    UC2

---
+++ src/srm/com/java/tictactoeUC2.java
@@ -0,0 +1,28 @@
+package srm.com.java;
+import java.util.Random;
+	public class tictactoeUC2 {
+	    static boolean isHumanTurn;
+	    static char humanSymbol;
+	    static char computerSymbol;
+
+	    public static void main(String[] args) {
+	        tossAndAssignSymbols();
+	        displayTossResult();
+	    }
+	    static void tossAndAssignSymbols(){
+	        Random random = new Random();
+	        isHumanTurn = random.nextBoolean(); // Randomly decide who goes first
+	        if (isHumanTurn) {
+	            humanSymbol = 'X';
+	            computerSymbol = 'O';
+	        } else {
+	            humanSymbol = 'O';
+	            computerSymbol = 'X';
+	        }
+	    }
+	    static void displayTossResult() {
+	        System.out.println("Human goes first: " + isHumanTurn);
+	        System.out.println("Human symbol: " + humanSymbol);
+	        System.out.println("Computer symbol: " + computerSymbol);
+	    }
+}
